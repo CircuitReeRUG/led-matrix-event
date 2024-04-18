7f99db7f35fa6c488aefffcf16b23bf051786557
Boyan <36108495+confestim@users.noreply.github.com>
2024-04-19 01:07:52 +0200

    Added TEX for presentation(up for debate). Added some ideas for content. Removed unnecessary code. Fixed typo in diagram filename.

--- src/lib.py
+++ src/lib.py
@@ -45,19 +45,16 @@ class Display:
         self.write(INTENSITY, value)  # Write the intensity value to the display
 
     # Refresh the display with data from the buffer
-    def show(self):
+    def __show(self):
         for x in range(8):
             self.cs.off()  # Set CS low to start transmission
             self.write(x + 1, self.buffer[x])  # Write column data
-            for t in range(self.size):
-                self.spi.write(
-                    bytearray([COLUMN + x, self.buffer[x + 8 * t]])
-                )  # Write pixel data
             self.cs.on()  # Set CS high to end transmission
 
     # Method to load a matrix of pixels into the buffer
     def set_pixels(self, pixels: bytearray):
         self.buffer = pixels  # Update the buffer with new pixel data
+        self.__show()  # Refresh the display with the new data
 
     # Method to set the state of a single pixel
     def set_pixel(self, x: int, y: int, value: int):
@@ -65,9 +62,11 @@ class Display:
             self.buffer[y] |= 1 << x  # Turn on the pixel by setting the bit
         else:
             self.buffer[y] &= ~(1 << x)  # Turn off the pixel by clearing the bit
+        self.__show()  # Refresh the display with the new data
 
     # Method to clear the display
     def clear(self):
         # Clear the buffer by setting all values to 0
         for i in range(len(self.buffer)):
             self.buffer[i] = 0
+        self.__show()
--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 from lib import Display
-from random import randint # Used by conway
+from random import randint  # Used by conway
 from utime import sleep
 
 # Create a Display object with SPI settings and number of cascaded devices
-display = Display(
-        spi=0, sck=2, mosi=3, cs=5, size=1
-    )  # SPI0, SCK=2, MOSI=3, CS=5, 1 device
+display = Display(spi=0, sck=2, mosi=3, cs=5, size=1)
 display.init()  # Initialize the display with default settings
 
 
@@ -35,7 +33,6 @@ def game_of_life():
                     new_matrix[x][y] = 1
         return new_matrix
 
-    
     def show_board(display) -> None:
         res = bytearray(8)
         for x in range(8):
@@ -43,7 +40,6 @@ def game_of_life():
                 if conway[x][y]:
                     res[y] |= 1 << x
         display.set_pixels(res)
-        
 
     # Main loop
     while True:
@@ -51,13 +47,12 @@ def game_of_life():
         conway = step(conway)  # Calculate the next generation
         # Create a byte array to store the pixel data
         show_board(display)  # Display it on the LED matrix
-        display.show()  # Update the physical display
-        sleep(.1)  # Wait a bit before the next generation
-        
-        
+        sleep(0.1)  # Wait a bit before the next generation
+
+
 def heart():
     # Create a Display object with SPI settings and number of cascaded devices
-    
+
     # Create a heart shape using a matrix of pixels
     heart = bytearray(
         [
@@ -75,16 +70,14 @@ def heart():
     # Display the heart shape on the display
     display.set_pixels(heart)
     # display.set_pixel(0, 0, 1)
-    display.show()
 
     # Delay for 5 seconds
     sleep(5)
 
     # Clear the display
     display.clear()
-    display.show()
 
-    
+
 if __name__ == "__main__":
     heart()
     game_of_life()
