13b4d3eb3fa55d3ca3956128e36f17ba2575c8c6
Boyan <36108495+confestim@users.noreply.github.com>
2024-05-02 02:10:27 +0200

    imperative code is now working and is a bit neater. Added pixel art lol.

--- src/imperative/main.py
+++ src/imperative/main.py
@@ -1,64 +1,70 @@
 from machine import Pin
-from utime import sleep
 
-# Connected:
-#   GND to GND
-#   V+ to VBus (for power from usb)
-#   DIN to GP0 
-#   CS to GP1 
-#   CLK to GP2
-
-DIN = Pin("GP0", Pin.OUT)
-CS = Pin("GP1", Pin.OUT)
+# Define GPIO pins
+DIN = Pin("GP3", Pin.OUT)
+CS = Pin("GP5", Pin.OUT)
 CLK = Pin("GP2", Pin.OUT)
 
-# Send the display test off command to the display
-# DISPLAYTEST = const(0x0F)  # Register for setting the display test mode
-# 0x0F = 0b00001111
-# 0b1 = display test mode on
-# 0b0 = display test mode off
-# 0b00001111 = 0x0F = 15
-#send_instruction([
+# Define command registers (these values need to be set according to your specific device's datasheet)
+DECODEMODE = 0x09
+SCANLIMIT = 0x0B
+SHUTDOWN = 0x0C
+DISPLAYTEST = 0x0F
+INTENSITY = 0x0A
 
-CLK.off()
-CS.on()
-# Could probably work with integers directly!
-def send_instruction(msg):
+def toggle_clock():
+    CLK.value(1)
+    CLK.value(0)
+
+def write(register, data):
     CS.off()
-    for bit in msg:
-        DIN.value(bit)
-        CLK.toggle()
-        CLK.toggle()
+    # Send register address (first send the high bit as 1 to indicate command)
+    for i in range(8):
+        DIN.value((register >> (7 - i)) & 1)
+        toggle_clock()
+
+    # Send data
+    for i in range(8):
+        DIN.value((data >> (7 - i)) & 1)
+        toggle_clock()
 
     CS.on()
+
+def init():
+    # Set up the matrix with the initial settings
+    instructions = [
+        (DECODEMODE, 0x00),
+        (SCANLIMIT, 7),
+        (SHUTDOWN, 1),
+        (DISPLAYTEST, 0),  # Setting display test to 1
+        (INTENSITY, 1),
+    ]
+    for cmd, data in instructions:
+        write(cmd, data)
+
+def set_image(image: bytearray):
+    for i, row in enumerate(image):
+        write(i + 1, row)
     
-send_instruction([0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1])
+    
 
-def set_image(image):
-    for col in range(1, 9):
-        data = [0,0,0,0]
+def main():
+    init() # <- initialize the display
+    
+    # Draw an "Among Us" crewmate
+    # Draw a cool pattern
+    cool_pattern = bytearray([
+        0b10000001,  # Row 1
+        0b01000010,  # Row 2
+        0b00100100,  # Row 3
+        0b00011000,  # Row 4
+        0b00011000,  # Row 5
+        0b00100100,  # Row 6
+        0b01000010,  # Row 7
+        0b10000001,  # Row 8
+    ])
 
-        data += [col & 8, col & 4, col & 2, col & 1]
-        data += image[8 * (col -1) : 8 * col]
 
-        send_instruction(data)
-
-set_image([
-    0,0,1,1,1,1,0,0,
-    0,1,0,0,0,0,1,0,
-    1,1,0,0,1,1,1,0,
-    1,1,0,1,0,0,0,1,
-    1,1,0,0,1,1,1,0,
-    1,1,0,0,0,0,1,0,
-    0,1,0,1,1,1,1,0,
-    0,1,1,1,0,1,1,0,
-    #
-    #0,0,1,1,1,0,0,0,
-    #0,1,0,0,0,1,0,0,
-    #1,1,0,0,1,1,0,0,
-    #1,1,0,1,0,0,1,0,
-    #1,1,0,0,1,1,0,0,
-    #1,1,0,0,0,1,0,0,
-    #0,1,0,1,0,1,0,0,
-    #0,1,1,0,1,1,0,0,
-])
+    set_image(cool_pattern)
+    
+main()
